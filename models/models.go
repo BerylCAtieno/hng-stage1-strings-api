45a79b22ac620b5d8221287c482cc8c03e8d7039
berylatieno <berylatieno30@gmail.com>
2025-10-20 15:04:37 +0300

    models

--- models/models.go
+++ models/models.go
@@ -2,9 +2,26 @@ package models
 
 import "github.com/BerylCAtieno/string-analyzer/analysis"
 
-type AnalysisReponsePayload struct {
+type StringReponsePayload struct {
 	Id         string              `json:"id"`
 	Value      string              `json:"value"`
 	Properties analysis.Properties `json:"properties"`
 	CreatedAt  string              `json:"created_at"`
+}
+
+type FilterPayload struct {
+	Data           []analysis.Properties `json:"data"`
+	Count          int                   `json:"count"`
+	FiltersApplied map[string]any        `json:"filters_applied"`
+}
+
+type InterpretedQuery struct {
+	Original      string                `json:"original"`
+	ParsedFilters []analysis.Properties `json:"parsed_filters"`
+}
+
+type NLPFilterPayload struct {
+	Data             []analysis.Properties `json:"data"`
+	Count            int                   `json:"count"`
+	InterpretedQuery InterpretedQuery      `'json:"interpreted_query"`
 }
