341a61b6a0acba28957759a5dddeabb91b5e5458
berylatieno <berylatieno30@gmail.com>
2025-10-20 14:21:47 +0300

    add error handling, make code more idiomatic

--- analysis/string_analysis.go
+++ analysis/string_analysis.go
@@ -1,34 +1,31 @@
 package analysis
 
-import "github.com/BerylCAtieno/string-analyzer/utils"
+import (
+	"errors"
 
+	"github.com/BerylCAtieno/string-analyzer/utils"
+)
+
+// Properties holds computed attributes of a string
 type Properties map[string]any
 
-type AnalysisPayload struct {
-	Id         string     `json:"id"`
-	Value      string     `json:"value"`
-	Properties Properties `json:"properties"`
-	CreatedAt  string     `json:"created_at"`
-}
+// AnalyzeString computes various attributes of an input string
+// Returns an error if the string is empty
+func AnalyzeString(s string) (Properties, error) {
 
-func AnalyzeString(s string) Properties {
+	if len([]rune(s)) == 0 {
+		return nil, errors.New("string cannot be empty")
+	}
 
-	properties := make(Properties)
+	properties := Properties{
+		"length ":                 utils.Length(s),
+		"is_palindrome":           utils.IsPalindrome(s),
+		"unique_characters":       utils.UniqueCharacters(s),
+		"word_count":              utils.NumberOfWords(s),
+		"sha256_hash":             utils.GenerateId(s),
+		"character_frequency_map": utils.CharFrequency(s),
+	}
 
-	length := utils.Length(s)
-	is_palindrome := utils.IsPalindrome(s)
-	unique_characters := utils.UniqueCharacters(s)
-	word_count := utils.NumberOfWords(s)
-	sha256_hash := utils.GenerateId(s)
-	character_frequency_map := utils.CharFrequency(s)
-
-	properties["length"] = length
-	properties["is_palindrome"] = is_palindrome
-	properties["unique_characters"] = unique_characters
-	properties["word_count"] = word_count
-	properties["sha256_hash"] = sha256_hash
-	properties["character_frequency_map"] = character_frequency_map
-
-	return properties
+	return properties, nil
 
 }
